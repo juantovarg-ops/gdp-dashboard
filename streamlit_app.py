0a723fbebb8d9dc10058368ee165726354607c47
juantovarg-ops <juantovarg-ops>
2025-09-19 05:01:01 +0000

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # -----------------------------------------------------------------------------
-# Declare some useful functions.
+# Declare some useful functions
 
 @st.cache_data
 def get_gdp_data():
